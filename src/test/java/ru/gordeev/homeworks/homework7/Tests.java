7841dbec20cefb1bb4c0d8ad940faa2171e3d335
Valentin <deathman777@gmail.com>
2023-11-20 01:25:32 +0500

    minor fix

--- src/test/java/ru/gordeev/homeworks/homework7/Tests.java
+++ src/test/java/ru/gordeev/homeworks/homework7/Tests.java
@@ -1,32 +1,34 @@
 package ru.gordeev.homeworks.homework7;
 
+import static ru.gordeev.homeworks.homework7.TerrainType.*;
+
 public class Tests {
 
     public static void main(String[] args) {
         Human human = new Human("Valentin", 10);
 
         human.setTransport(new Car(100));
-        human.move(100, TerrainType.FLATLAND);
+        human.move(100, FLATLAND);
 
         human.setTransport(new Horse(10));
-        human.move(20, TerrainType.DEEP_FOREST);
+        human.move(20, DEEP_FOREST);
 
         human.setTransport(new Rover(10));
-        human.move(30, TerrainType.SWAMP);
+        human.move(30, SWAMP);
 
         human.setTransport(new Bike(human));
-        human.move(20, TerrainType.FLATLAND);
+        human.move(20, FLATLAND);
 
         human.setTransport(new Car(-100));
-        human.move(100, TerrainType.FLATLAND);
+        human.move(100, FLATLAND);
 
         human.setTransport(new Horse(0));
-        human.move(20, TerrainType.DEEP_FOREST);
+        human.move(20, DEEP_FOREST);
 
         human.setTransport(new Rover(-100));
-        human.move(30, TerrainType.SWAMP);
+        human.move(30, SWAMP);
 
         human.setTransport(new Bike(human));
-        human.move(-20, TerrainType.FLATLAND);
+        human.move(-20, FLATLAND);
     }
 }
